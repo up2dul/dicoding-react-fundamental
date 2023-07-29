4e688645a47c457ffa4baffad9ed8053100db6b4
abdul <malik301002@gmail.com>
2023-07-29 10:42:46 +0700

    chore: extend plugin and add some rules

--- submission-1/.eslintrc.cjs
+++ submission-1/.eslintrc.cjs
@@ -6,12 +6,18 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'quotes': ['warn', 'single'],
+    'semi': ['warn', 'always'],
+    'no-unused-vars': 1,
+		'indent': ['warn', 2],
+    'no-multiple-empty-lines': ['warn', { 'max': 2, 'maxEOF': 0 }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
