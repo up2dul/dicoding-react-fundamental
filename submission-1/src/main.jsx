5a4b94036b20285a292f797b2fff2bd6b36f1ac5
abdul <malik301002@gmail.com>
2023-08-09 23:53:00 +0700

    feat: wrap App with BrowserRouter

--- submission-1/src/main.jsx
+++ submission-1/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/App.jsx';
 import theme from '@/styles/theme';
@@ -8,8 +9,10 @@ import '@/styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
