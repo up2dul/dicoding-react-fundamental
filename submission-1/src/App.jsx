475a28e1564a0c5a6fd2ef018be1e9ab6191718b
up2dul <malik301002@gmail.com>
2023-08-10 01:21:01 +0700

    feat: wrap pages components with Routes

--- submission-1/src/App.jsx
+++ submission-1/src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+
 import Layout from '@/components/layout/Layout';
+import Home from '@/pages/Home';
+import Archive from '@/pages/Archive';
+import NotFound from '@/pages/NotFound';
 
 export default function App() {
   return (
     <Layout>
-      <h1>Hello world</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/archive' element={<Archive />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </Layout>
   );
 }
