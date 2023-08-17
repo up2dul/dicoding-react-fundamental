059f3e924a71d34ea4d3f17a876c9cad869bcd1d
up2dul <malik301002@gmail.com>
2023-08-18 00:06:13 +0700

    feat(lib): create utils file

---
+++ submission-1/src/lib/utils.js
@@ -0,0 +1,11 @@
+function formatDate(date) {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  return new Date(date).toLocaleDateString('en-EN', options);
+}
+
+export { formatDate };
