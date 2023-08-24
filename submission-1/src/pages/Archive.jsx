3e2b3faf2a17a3a60d75497359f2c99c03e8ee91
up2dul <malik301002@gmail.com>
2023-08-24 11:40:01 +0700

    feat(pages): add InputSearch component

--- submission-1/src/pages/Archive.jsx
+++ submission-1/src/pages/Archive.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import { Heading, ListItem, UnorderedList } from '@chakra-ui/react';
 
 import Card from '@/components/common/Card';
+import InputSearch from '@/components/common/InputSearch';
 import Layout from '@/components/layout/Layout';
 import { getArchivedNotes } from '@/lib/local-data';
 
 export default function Archive() {
-  const notes = getArchivedNotes();
+  const initialNotes = getArchivedNotes();
+  const [notes, setNotes] = useState(initialNotes);
 
   return (
     <Layout>
       <Heading as='h1'>Archived notes</Heading>
+
+      <InputSearch
+        initialNotes={initialNotes}
+        onChange={(filteredNotes) => setNotes(filteredNotes)}
+      />
 
       {notes.length > 0 ? (
         <UnorderedList spacing={5} styleType='none'>
@@ -21,7 +29,7 @@ export default function Archive() {
         </UnorderedList>
       ) : (
         <Heading as='h1' size='md'>
-          No notes are archived.
+          No archived notes found.
         </Heading>
       )}
     </Layout>
--- submission-1/src/pages/Home.jsx
+++ submission-1/src/pages/Home.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import { Heading, ListItem, UnorderedList } from '@chakra-ui/react';
 
 import Card from '@/components/common/Card';
+import InputSearch from '@/components/common/InputSearch';
 import Layout from '@/components/layout/Layout';
 import { getActiveNotes } from '@/lib/local-data';
 
 export default function Home() {
-  const notes = getActiveNotes();
+  const initialNotes = getActiveNotes();
+  const [notes, setNotes] = useState(initialNotes);
 
   return (
     <Layout>
       <Heading as='h1'>Active notes</Heading>
+
+      <InputSearch
+        initialNotes={initialNotes}
+        onChange={(filteredNotes) => setNotes(filteredNotes)}
+      />
 
       {notes.length > 0 ? (
         <UnorderedList spacing={5} styleType='none'>
@@ -21,7 +29,7 @@ export default function Home() {
         </UnorderedList>
       ) : (
         <Heading as='h1' size='md'>
-          No notes are active.
+          No active notes found.
         </Heading>
       )}
     </Layout>
