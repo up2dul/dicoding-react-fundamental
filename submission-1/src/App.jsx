f61020cd9da53ccc2a6a5617868c1fe3a86e7b4f
Abdul <malik301002@gmail.com>
2023-09-09 10:46:59 +0700

    fix(sub-1): multiple Layout

--- submission-1/src/App.jsx
+++ submission-1/src/App.jsx
@@ -1,22 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 
-import Layout from '@/components/layout/Layout';
-import Home from '@/pages/Home';
-import Archive from '@/pages/Archive';
-import Detail from '@/pages/Detail';
-import Add from '@/pages/Add';
-import NotFound from '@/pages/NotFound';
+import { Add, Archive, Detail, Home, NotFound } from '@/pages';
 
 export default function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/archive' element={<Archive />} />
-        <Route path='/detail/:noteId' element={<Detail />} />
-        <Route path='/add' element={<Add />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/archive' element={<Archive />} />
+      <Route path='/detail/:noteId' element={<Detail />} />
+      <Route path='/add' element={<Add />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
     </Layout>
   );
 }
