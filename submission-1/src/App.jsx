80d933a82b4af86240f97fb3671babc12354a81f
Abdul <malik301002@gmail.com>
2023-09-09 10:54:26 +0700

    tf idk why is this code still here

--- submission-1/src/App.jsx
+++ submission-1/src/App.jsx
@@ -4,7 +4,6 @@ import { Add, Archive, Detail, Home, NotFound } from '@/pages';
 
 export default function App() {
   return (
-    <Layout>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/archive' element={<Archive />} />
@@ -12,6 +11,5 @@ export default function App() {
       <Route path='/add' element={<Add />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
-    </Layout>
   );
 }
